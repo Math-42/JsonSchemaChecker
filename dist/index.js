0a0f1b41c9e71cf5c2cc01a69527cb37e1743873
Math-42 <matheus.v4200@gmail.com>
2021-06-14 13:37:15 -0300

    adding dist folder

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";/************************************************************************/
+var __webpack_exports__ = {};
+async function test(){
+	console.log("it's alive");
+}
+
+test();
+module.exports = __webpack_exports__;
+/******/ })()
+;
